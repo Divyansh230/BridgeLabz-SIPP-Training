354812929dd4dc2d898e26954f3370153132b971
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-06-13 10:20:05 +0530

    LeapYear is calculated

---
+++ Day_04/LeapYear.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt(); // Example input
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true; // Divisible by 400
+                }
+            } else {
+                isLeapYear = true; // Divisible by 4 but not by 100
+            }
+        }
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
