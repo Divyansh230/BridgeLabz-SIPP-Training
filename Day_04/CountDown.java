8f4f4b7edc9cbcf59e7556fdf15fb0f6a3e2361c
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-06-13 09:49:05 +0530

    CountDown finished

---
+++ Day_04/CountDown.java
@@ -0,0 +1,11 @@
+public class CountDown {
+    public static void main(String[] args) {
+        int n = 10; // Starting number for countdown
+        while (n >= 0) {
+            System.out.println(n);
+            n--; // Decrement the number
+        }
+        System.out.println("Countdown finished!");
+    }
+    
+}
