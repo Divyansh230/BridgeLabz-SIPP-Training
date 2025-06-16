89c8dff0a67919fcd698f038dad7b3e09f393475
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-06-16 09:25:24 +0530

    Core_java

--- Arrays/Votting.java
+++ Arrays/Votting.java
@@ -6,7 +6,7 @@ public class Votting {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ages[] = new int[2];
+        int ages[] = new int[10];
         for (int i = 0; i < ages.length; i++) {
             ages[i] = sc.nextInt();
         }
