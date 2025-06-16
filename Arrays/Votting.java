2e79986789e2d712a9e41514fd3b023af1335939
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-06-16 09:20:34 +0530

    Arrays of Ages

---
+++ Arrays/Votting.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.*;
+
+public class Votting {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ages[] = new int[2];
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] >= 18) {
+                System.out.println("Student " + (i + 1) + " can vote");
+            }
+        }
+    }
+}
