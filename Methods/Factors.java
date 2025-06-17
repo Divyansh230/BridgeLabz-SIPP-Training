62ad9ec3f222bd7493e554d50bd06850ac6e92a7
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-06-17 09:47:01 +0530

    Factors are calculated

---
+++ Methods/Factors.java
@@ -0,0 +1,55 @@
+package Methods;
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[] factors = findFactors(num);
+
+        System.out.print("Factors: ");
+        for (int f : factors) {
+            System.out.print(f + " ");
+        }
+        System.out.println();
+
+        System.out.println("Sum of factors: " + sumFactors(factors));
+        System.out.println("Product of factors: " + productFactors(factors));
+        System.out.println("Sum of squares of factors: " + sumOfSquares(factors));
+
+        sc.close();
+    }
+
+    static int[] findFactors(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+
+        int[] factors = new int[count];
+        int idx = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) factors[idx++] = i;
+        }
+        return factors;
+    }
+
+    static int sumFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    static int productFactors(int[] factors) {
+        int product = 1;
+        for (int f : factors) product *= f;
+        return product;
+    }
+
+    static int sumOfSquares(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += Math.pow(f, 2);
+        return sum;
+    }
+}
