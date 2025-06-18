4d218d066f42637a7ba910bb46341497ae07a32d
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-06-18 16:26:15 +0530

    Hello

---
+++ Strings_and_Exceptional_Handling/CharrArra.java
@@ -0,0 +1,11 @@
+package Strings_and_Exceptional_Handling;
+import java.util.*;
+public class CharrArra {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+
+        char c[]=str.toCharArray();
+        System.out.println(Arrays.toString(c));
+    }
+}
